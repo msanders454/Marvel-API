f0a8cb911c93fcfe1570d5fd23a75bd4a19af0d4
Michael Sanders <mikesanderspsu@gmail.com>
2020-12-03 17:05:24 -0500

    Hide Keys

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+import dotenv from 'dotenv'
+dotenv.config()
 const privateKey = process.env.privateKey
 //Marvel API Gateway
 const marvelAPIURL = 'https://gateway.marvel.com/v1/public/characters?';
